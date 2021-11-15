d58e69dfc135d3e41dfa9c362d5c4cdda0dac33a
Roberto Mejía <robenriquemejia@gmail.com>
2021-11-15 17:31:13 -0600

    2

--- app/src/main/java/com/example/pm01ej21/MainActivity.java
+++ app/src/main/java/com/example/pm01ej21/MainActivity.java
@@ -1,14 +1,71 @@
 package com.example.pm01ej21;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int CAMERA_PERMISSION_CODE = 100;
+    int VIDEO_REQUEST = 1111;
+    Uri videoUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnGrabar = (Button) findViewById(R.id.btnGrabar);
+        Button btnGuardados = (Button) findViewById(R.id.btnGuardados);
+
+        CameraPermission();
+
+        btnGrabar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recordVideo();
+            }
+        });
+    }
+
+    public void CameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+        }
+    }
+
+    public void recordVideo() {
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        startActivityForResult(intent, VIDEO_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == VIDEO_REQUEST && resultCode == RESULT_OK) {
+            videoUri = data.getData();
+            Toast.makeText(this, "Video Grabado/Guardado", Toast.LENGTH_SHORT).show();
+
+            VideoView videoView = findViewById(R.id.videoView);
+            videoView.setVideoURI(data.getData());
+            videoView.start();
+        } else {
+            Toast.makeText(this, "Video NO Grabado/Guardado", Toast.LENGTH_SHORT).show();
+        }
     }
 }
